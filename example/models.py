55664deb4fb35028c66cb18c7994166c49402f97
unknown <Python@BUE302D06.alunos.sc.senac.br>
2021-01-19 17:24:02 -0300

    inicial

--- example/models.py
+++ example/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=None, blank=True)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES, blank=True)
+
+class Musician(Person):
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+
