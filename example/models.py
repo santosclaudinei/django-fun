5ce875ab7c21672a130ceba7876b9df84dc21b67
unknown <Python@BUE302D06.alunos.sc.senac.br>
2021-01-20 17:18:38 -0300

    atualização 20_01_2020

--- example/models.py
+++ example/models.py
@@ -8,10 +8,22 @@ class Reporter(models.Model):
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
 
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     headline = models.CharField(max_length=100)
     pub_date = models.DateField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication)
 
     def __str__(self):
         return self.headline
